68fcc873953ac46e203656232a6c611da8cc1e25
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-11-06 00:33:57 +0200

    add step 5

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const getDataUsers = await readContacts();
+
+    return JSON.parse(getDataUsers);
+  } catch (error) {
+    throw new error(error.message);
+  }
+};
 
 console.log(await getAllContacts());
