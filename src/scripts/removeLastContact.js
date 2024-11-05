2a469ac100ea2a5f38b97b9e669870910769a48d
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-11-06 01:41:09 +0200

    add step 8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,19 @@
-export const removeLastContact = async () => {};
+import { countContacts } from './countContacts.js';
+import { getAllContacts } from './getAllContacts.js';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const removeLastContact = async () => {
+  try {
+    const allContacts = await getAllContacts();
+    const numberAllcontact = await countContacts();
+    if (numberAllcontact > 0) {
+      const updateData = allContacts.slice(0, numberAllcontact - 1);
+      await fs.writeFile(PATH_DB, JSON.stringify(updateData, null, 2));
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
 removeLastContact();
